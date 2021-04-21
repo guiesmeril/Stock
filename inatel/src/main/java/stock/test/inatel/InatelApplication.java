f284f524e683f046d2187cd0e749628ed66c1157
Guilherme Cesar da Silva <guiesmeril@hotmail.com>
2021-04-21 17:57:01 -0300

    Subindo o restante dos arquivos

---
+++ inatel/src/main/java/stock/test/inatel/InatelApplication.java
@@ -0,0 +1,13 @@
+package stock.test.inatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InatelApplication.class, args);
+	}
+
+}
---
+++ inatel/src/test/java/stock/test/inatel/InatelApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.test.inatel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
